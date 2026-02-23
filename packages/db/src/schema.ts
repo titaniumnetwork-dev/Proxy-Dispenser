325be009fea222c16ecca9ba0b28e9d1f9b6f0e0
Ryan Wilson <inbox@ryanwilson.space>
2026-02-23 09:32:48 -0500

    feat(db)!: Update DB schema for upcoming features, such as for the config system and dispense modes
    - Add guild_cohorts table for possibly upcoming smart link distribution systems
    - Add dispense_mode and automatic_dispense fields to the guild table for upcoming link distribution systems
    - Rename users -> guild_users for consistency and to prevent migrations in the future, since a global_users table is planned
    - Remove the unnecessary global_banned_users table
    - Add per-category log channels (blocked link reports and cohort approvals) for upcoming changes
    
    BREAKING CHANGE: DB schema updated; migration required

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -7,6 +7,27 @@ import {
 	text,
 } from "drizzle-orm/sqlite-core";
 
+export type Filters = {
+	[key: string]: {
+		id: string;
+		logoUrl: string;
+	};
+};
+export const filters: Filters = {
+	"": {
+		id: "",
+		logoUrl: "",
+	},
+};
+
+const filtersEnum = (name: string) =>
+	text(name, {
+		enum: Object.entries(filters).map(([_, value]) => value.id) as [
+			string,
+			...string[],
+		],
+	});
+
 const snowflake = (name: string) => text(name);
 
 export const guild = sqliteTable("guild", {
@@ -24,7 +45,10 @@ export const guild = sqliteTable("guild", {
 		.$type<string[]>()
 		.notNull()
 		.default(sql`(json_array())`),
+	// This is a generic log channel for the bot to send messages to. These are where the logs will be sent if there is no custom log channel set up for a specific category.
 	logChannelId: snowflake("log_channel_id"),
+	logChannelBlockedLinkReports: snowflake("log_channel_blocked_link_reports"),
+	logChannelCohortApprovals: snowflake("log_channel_cohort_approvals"),
 	monthlyCycle: text("monthly_cycle", {
 		enum: ["first_of_month", "relative_to_user"],
 	})
@@ -33,6 +57,24 @@ export const guild = sqliteTable("guild", {
 	isBanned: integer("is_banned").notNull().default(0),
 	customByodHost: text("custom_byod_host"),
 	customByodHostAPIKey: text("custom_byod_host_api_key"),
+	dispenseMode: text("dispense_mode", {
+		enum: ["traditional", "cohort"],
+	})
+		.notNull()
+		.default("cohort"),
+	// Automatically dispense links to the user when a link gets blocked
+	// It will check daily, but there will be a "Report Blocked" button when DMing or responding to the user's command/panel
+	// This report blocked button will trigger for everyone in the cohort to have their links checked, and if it is blocked, give the user a new one
+	automaticDispense: integer("automatic_dispense").notNull().default(0),
+});
+
+export const guildCohorts = sqliteTable("guild_cohorts", {
+	// The primary key is either `custom_enterprise_policy_domain` or `filters`
+	customEnterprisePolicyDomain: text("custom_enterprise_policy_domain"),
+	filters: text("filters", { mode: "json" })
+		.$type<string[]>()
+		.notNull()
+		.default(sql`(json_array())`),
 });
 
 export const guildConfigHistory = sqliteTable(
@@ -55,22 +97,12 @@ export const guildConfigHistory = sqliteTable(
 	],
 );
 
-export const globalBannedUsers = sqliteTable("global_banned_users", {
-	userId: snowflake("user_id").notNull().primaryKey(),
-	banned: integer("banned").notNull().default(0),
-});
-
-export const users = sqliteTable(
+export const guildUsers = sqliteTable(
 	// Excuse the verbosity, I plan to also have a "global_users" table for the planned global features
 	"guild_users",
 	{
 		guildId: snowflake("guild_id").notNull(),
 		userId: snowflake("user_id").notNull(),
-		chosenFilters: text("chosen_filters", { mode: "json" })
-			.$type<string[]>()
-			.notNull()
-			.default(sql`(json_array())`),
-		chosenCategory: text("chosen_category").notNull(),
 		receivedLinks: text("received_links", { mode: "json" })
 			.$type<string[]>()
 			.notNull()
@@ -81,7 +113,7 @@ export const users = sqliteTable(
 		firstTimeUserCycleTimestamp: integer("first_time_user_cycle_timestamp", {
 			mode: "timestamp_ms",
 		}),
-		banned: integer("banned").notNull().default(0),
+		isBanned: integer("is_banned").notNull().default(0),
 	},
 	(t) => [
 		primaryKey({ columns: [t.guildId, t.userId] }),
@@ -92,6 +124,38 @@ export const users = sqliteTable(
 		}).onDelete("cascade"),
 	],
 );
+
+export const globalConfig = sqliteTable("global_config", {
+	botOwnerIds: text("bot_owner_ids", { mode: "json" })
+		.$type<string[]>()
+		.notNull()
+		.default(sql`(json_array())`),
+});
+
+export const globalUsers = sqliteTable("global_users", {
+	userId: snowflake("user_id").notNull().primaryKey(),
+	// For known link leakers
+	// TODO: Will be implemented later during bot-owner commands
+	isBanned: integer("is_banned").notNull().default(0),
+	// TODO: Update all references to use it on the global users table
+	// TODO: Use an enum for the chosen filters
+	// Maybe? profile id?
+	customEnterprisePolicyDomain: text("custom_enterprise_policy_domain"),
+	chosenFilters: text("filters", { mode: "json" })
+		.$type<string[]>()
+		.notNull()
+		.default(sql`(json_array())`),
+	subscribedGuilds: text("subscribed_guilds", { mode: "json" })
+		.$type<string[]>()
+		.notNull()
+		// Array of guild ids
+		.default(sql`(json_array())`),
+	/**
+	 * From the chrome policy (chrome://policy)
+	 * This can be from:
+	 * status.user.domain, status.user.enterpriseDomainManager
+	 */
+});
 
 export const categories = sqliteTable(
 	"categories",
@@ -117,6 +181,10 @@ export const links = sqliteTable(
 		guildId: snowflake("guild_id").notNull(),
 		categoryId: text("category_id").notNull(),
 		link: text("link").notNull(),
+		blockedFilters: filtersEnum("blocked_filters"),
+		lastBlockCheckTimestamp: integer("last_blocked_check_timestamp", {
+			mode: "timestamp_ms",
+		}),
 	},
 	(t) => [
 		foreignKey({
@@ -133,15 +201,15 @@ export const links = sqliteTable(
 );
 
 // Relations
-export const usersRelations = relations(users, ({ one }) => ({
+export const usersRelations = relations(guildUsers, ({ one }) => ({
 	guild: one(guild, {
-		fields: [users.guildId],
+		fields: [guildUsers.guildId],
 		references: [guild.guildId],
 	}),
 }));
 
 export const guildRelations = relations(guild, ({ many }) => ({
-	users: many(users),
+	users: many(guildUsers),
 	categories: many(categories),
 	links: many(links),
 	configHistory: many(guildConfigHistory),
@@ -183,11 +251,14 @@ export type SelectGuild = typeof guild.$inferSelect;
 export type InsertGuildConfigHistory = typeof guildConfigHistory.$inferInsert;
 export type SelectGuildConfigHistory = typeof guildConfigHistory.$inferSelect;
 
-export type InsertUser = typeof users.$inferInsert;
-export type SelectUser = typeof users.$inferSelect;
+export type InsertUser = typeof guildUsers.$inferInsert;
+export type SelectUser = typeof guildUsers.$inferSelect;
 
-export type InsertGlobalBannedUser = typeof globalBannedUsers.$inferInsert;
-export type SelectGlobalBannedUser = typeof globalBannedUsers.$inferSelect;
+export type InsertGlobalConfig = typeof globalConfig.$inferInsert;
+export type SelectGlobalConfig = typeof globalConfig.$inferSelect;
+
+export type InsertGlobalUser = typeof globalUsers.$inferInsert;
+export type SelectGlobalUser = typeof globalUsers.$inferSelect;
 
 export type InsertCategory = typeof categories.$inferInsert;
 export type SelectCategory = typeof categories.$inferSelect;
