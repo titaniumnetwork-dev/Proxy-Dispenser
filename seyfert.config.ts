90282407b1f17e3fe9d24bb3d3e75ec2726aa0f1
percs <me@percs.dev>
2026-04-12 17:15:04 -0500

    do not throw on missing bot token

--- seyfert.config.ts
+++ seyfert.config.ts
@@ -1,11 +1,7 @@
 import { config } from "seyfert";
 
-if (!process.env.BOT_TOKEN) {
-	throw new Error("Missing Discord Bot Token!");
-}
-
 export default config.bot({
-	token: process.env.BOT_TOKEN,
+	token: process.env.BOT_TOKEN!,
 	locations: {
 		base: "src",
 		commands: "commands",
