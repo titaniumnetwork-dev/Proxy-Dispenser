4fb14fc8a136962e0c40459e66f926484fbdaf0f
percs <me@percs.dev>
2026-04-16 23:23:46 -0500

    dont do top level error handling

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,6 @@ import { Client } from "seyfert";
 
 const client = new Client();
 
-process.on("unhandledRejection", (error) => {
-	console.error("Unhandled promise rejection:", error);
-});
-
-process.on("uncaughtException", (error) => {
-	console.error("Uncaught exception:", error);
-});
-
 client.start().then(
 	() => client.uploadCommands({ cachePath: "./commands.json" }),
 	(error) => {
