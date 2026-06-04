7cf341842e56f914efe1b91ec48b33f415d21ac9
Sophia <sophiaasophieee@gmail.com>
2026-06-04 08:18:41 -0400

    Update admin.ts

--- src/commands/admin.ts
+++ src/commands/admin.ts
@@ -2,6 +2,7 @@ import BlacklistCommand from "@commands/admin/blacklist";
 import ResetCommand from "@commands/admin/reset";
 import ResetUserCommand from "@commands/admin/resetUser";
 import UnblacklistCommand from "@commands/admin/unblacklist";
+import SayCommand from "@commands/admin/say";
 import { Command, Declare, Options } from "seyfert";
 
 @Declare({
@@ -11,5 +12,5 @@ import { Command, Declare, Options } from "seyfert";
 	contexts: ["Guild"],
 	defaultMemberPermissions: 0n,
 })
-@Options([BlacklistCommand, UnblacklistCommand, ResetCommand, ResetUserCommand])
+@Options([BlacklistCommand, UnblacklistCommand, ResetCommand, ResetUserCommand, SayCommand])
 export default class AdminCommand extends Command {}
